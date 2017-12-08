7912a363740b267fc45760078dfa35e6dc5ee4b5
OrangeXC <842012622@qq.com>
2017-12-08 12:29:30 +0800

    Change ui from bootstrap to material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core'
 import { Injectable } from '@angular/core'
 import { HttpClientModule, HttpRequest, HttpInterceptor, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { Routes, RouterModule }   from '@angular/router'
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { LoadingModule } from 'ngx-loading'
 import { Observable } from 'rxjs/Observable'
 
 import { AppComponent } from './app.component'
-import { AppNavbarComponent } from './app-navbar/app-navbar.component'
 import { TranslateComponent } from './translate/translate.component'
 import { HomeComponent } from './home/home.component'
 import { SearchComponent } from './search/search.component'
@@ -18,6 +17,42 @@ import { DetailWebTransComponent } from './detail-web-trans/detail-web-trans.com
 import { DetailAuthTransComponent } from './detail-auth-trans/detail-auth-trans.component'
 import { DetailTransformComponent } from './detail-transform/detail-transform.component'
 import { DetailExamplesComponent } from './detail-examples/detail-examples.component'
+
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+} from '@angular/material';
+
+import { CdkTableModule } from '@angular/cdk/table';
 
 const routes: Routes = [
   {
@@ -49,9 +84,46 @@ export class ExampleInterceptor implements HttpInterceptor {
 }
 
 @NgModule({
+  exports: [
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+  ]
+})
+export class MaterialModule {}
+
+@NgModule({
   declarations: [
     AppComponent,
-    AppNavbarComponent,
     TranslateComponent,
     HomeComponent,
     SearchComponent,
@@ -64,9 +136,10 @@ export class ExampleInterceptor implements HttpInterceptor {
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     RouterModule.forRoot(routes),
-    NgbModule.forRoot(),
+    MaterialModule,
     LoadingModule
   ],
   providers: [
