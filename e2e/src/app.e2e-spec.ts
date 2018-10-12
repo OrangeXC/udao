357bb8575530ad8f389bca496f9058d6f2652461
OrangeXC <842012622@qq.com>
2018-10-12 16:15:01 +0800

    Angular 7.0.0-rc.2

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AppPage } from './app.po';
+'use strict'; // necessary for es6 output in node
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+import { browser, element, by } from 'protractor';
 
+describe('cli-quickstart App', () => {
   beforeEach(() => {
-    page = new AppPage();
+    return browser.get('/');
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should display message saying app works', () => {
+    const pageTitle = element(by.css('app-root h1')).getText();
+    expect(pageTitle).toEqual('Welcome to My First Angular App!!');
   });
 });
